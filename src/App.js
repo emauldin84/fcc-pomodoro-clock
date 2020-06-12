4d472a326d79695531756fbe21d36f7a397b8e66
Eric Mauldin <emauldin84@gmail.com>
2020-06-12 16:01:35 -0400

    tinkers with setInterval and builds useAsyncState hook. still not working correctly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React, {useState} from 'react';
 import './App.css';
 
+import useInterval from './utils/useInterval'
+
 function App() {
 
   const [breakLength, setBreakLength] = useState(5)
   const [sessionLength, setSessionLength] = useState(25)
   const [inSession, setInSession] = useState(true)
+  // const [seconds, setSeconds] = useState(1500)
+  const [timerRunning, setTimerRunning] = useState(false)
+  const [timer, setTimer] = useState(null)
+  
+  // const [displayMins, setDisplayMins] = useState(25)
+  // const [displaySecs, setDisplaySecs] = useState(0)
+  
+  const useAsyncState = (initValue) => {
+    const [value, setValue] = useState(initValue)
+    const setter = x => 
+      new Promise(resolve => {
+        setValue(x)
+        resolve(x)
+      })
+      return [value, setter]
+  }
+  const [seconds, setSeconds] = useAsyncState(1500)
+  const [displayMins, setDisplayMins] = useAsyncState(25)
+  const [displaySecs, setDisplaySecs] = useAsyncState(0)
+
+  const displayCount = () => {
+    // if (inSession) setSeconds(sessionLength * 60)
+    // if (!inSession) setSeconds(breakLength * 60)
+    // setSeconds(seconds - 1)
+    // setDisplayMins(Math.floor(((seconds - 1) % 3600)/60))
+    // setDisplaySecs(Math.floor((seconds - 1) % 60))
+    
+    setSeconds(seconds - 1)
+      .then(seconds => {
+        console.log(seconds)
+        setDisplayMins(Math.floor((seconds % 3600)/60))
+        setDisplaySecs(Math.floor(seconds % 60))
+
+      })
+  }
 
   const handleBreakIncrememnt = () => {
     if (breakLength <= 59){
@@ -29,11 +66,41 @@ function App() {
       setSessionLength(prevState => prevState -= 1)
     }
   }
-
+  
   const handleStartStopTimer = () => {
-      let timer = setInterval(handleSessionDecrement, 1000)
+    if (!timerRunning){
+      setTimerRunning(true)
+      // setTimer(setInterval(() => {
+      //   setSeconds(seconds - 1)
+      //     .then(seconds => {
+      //       console.log(seconds)
+      //       setDisplayMins(Math.floor((seconds % 3600)/60))
+      //       setDisplaySecs(Math.floor(seconds % 60))
+      // })
+      // }, 1000)
+      // )
+      while (seconds > 0) {
+        setTimer(setInterval(() => {
+            setSeconds(seconds - 1)
+              .then(seconds => {
+                console.log(seconds)
+                setDisplayMins(Math.floor((seconds % 3600)/60))
+                setDisplaySecs(Math.floor(seconds % 60))
+          })
+          }, 1000)
+        )
+    }}
 
-  }
+    if (timerRunning){
+      setTimerRunning(false)
+      setTimer(clearTimeout(timer))
+    }
+
+}
+
+
+
+  // let minsDisplay = inSession ? sessionLength : breakLength
 
   return (
     <div className="App">
@@ -55,8 +122,8 @@ function App() {
       {/* Timer Display */}
       <div className='timer-container'>
         <p id='timer-label'>Session</p>
-        <p id='time-left'>mm:ss</p>
-        <p id='start-stop' onClick={handleStartStopTimer}>start/stop</p>
+        <p id='time-left'>{displayMins <= 9 ? '0' : null}{displayMins}:{displaySecs <= 9 ? '0' : null}{displaySecs}</p>
+        <p id='start_stop' onClick={handleStartStopTimer}>{timerRunning ? 'stop':'start'}</p>
         <p id='reset'>reset</p>  {/*When I click the element with the id of reset, any running timer should be stopped, the value within id="break-length" should return to 5, the value within id="session-length" should return to 25, and the element with id="time-left" should reset to it's default state.*/}
       </div>
       
