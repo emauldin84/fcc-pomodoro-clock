ef711a9033aec560bf4cb7558a2e8be524be0b12
Eric Mauldin <emauldin84@gmail.com>
2020-06-16 11:44:55 -0400

    cleans up spacing in TimeLeft.tsx

--- src/components/TimeLeft.tsx
+++ src/components/TimeLeft.tsx
@@ -13,7 +13,7 @@ const TimeLeft: React.FC<Props> = ({handleStartStopClick, timerLabel, startStopB
     return (
         <div className="flex flex-col justify-evenly items-center w-64 h-64 bg-red-600 rounded-full">
             <p className="text-red-900 text-2xl" id='timer-label'>{timerLabel}</p>
-            <p className="font-clock text-4xl font-bold"id='time-left'>{formattedTimeLeft}</p>
+            <p className="font-clock text-4xl font-bold" id='time-left'>{formattedTimeLeft}</p>
             <button className="text-red-400 font-semibold bg-green-900 px-4 py-2 rounded-lg" id='start_stop' onClick={handleStartStopClick}>{startStopButtonLabel}</button>
             <button className="border-2 text-green-900 rounded-lg border-green-900 border-solid px-4 py-2" id='reset' onClick={handleResetButtonClick}>reset</button>
         </div>
